afc615ee201d51ef316e1fae8e60f2c1065fcd76
fatbobman <xuyang@me.com>
2024-09-28 10:32:31 +0800

    add _exported，简化导入

---
+++ Sources/CoreDataEvolution/module.swift
@@ -0,0 +1,12 @@
+//
+//  ------------------------------------------------
+//  Original project: CoreDataEvolution
+//  Created on 2024/9/28 by Fatbobman(东坡肘子)
+//  X: @fatbobman
+//  Mastodon: @fatbobman@mastodon.social
+//  GitHub: @fatbobman
+//  Blog: https://fatbobman.com
+//  ------------------------------------------------
+//  Copyright © 2024-present Fatbobman. All rights reserved.
+
+@_exported import CoreData
