9d3efca2b47737389b6f77ea88a9463a2d99ed79
fatbobman <xuyang@me.com>
2024-10-06 16:53:47 +0800

    update

--- Package.swift
+++ Package.swift
@@ -31,7 +31,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CoreDataEvolution"
+            name: "CoreDataEvolution",
+            dependencies: [
+                "CoreDataEvolutionMacros"
+            ]
         ),
         .testTarget(
             name: "CoreDataEvolutionTests",
