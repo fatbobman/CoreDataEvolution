2303b85dc4d370f9deea077f64729aaea4520664
fatbobman <xuyang@me.com>
2025-05-07 15:22:32 +0800

    更新 .gitignore 文件以包含 .swiftpm 和 .vscode 目录，更新 Package.resolved 中 swift-syntax 的来源和版本号，修改 Package.swift 中的依赖 URL。

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
